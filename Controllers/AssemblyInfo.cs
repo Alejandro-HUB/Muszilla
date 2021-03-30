e5356cd5934360776cd4cbeac9a54f1d21398e83
Micheal <Micheal@W10-MAZC>
2021-03-29 21:04:53 -0400

    Added Selects to AInfo file

---
+++ Controllers/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Runtime.InteropServices;
+/*select TOP(100)
+User_ID,USERNAME,FirstName,LastName,Email,Pass_word,CreatedDate,Old_Pass_word
+from Consumer
+
+select TOP(100)
+Playlist_ID,Playlist_Name,User_ID_FK
+from Playlist
+
+select TOP(100)
+playlistID,songsID
+from SongsPlaylistR
+
+ * 
+ * 
+ */
