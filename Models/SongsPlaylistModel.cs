66ecc5f7ccf5ceb652135e20f59776f0999d7306
Alejandro Lopez <al2151@mynsu.nova.edu>
2021-03-29 21:01:40 -0400

    Added Consumer, Playlist, Songs, etc models

---
+++ Models/SongsPlaylistModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muszilla.Models
+{
+    public class SongsPlaylistModel
+    {
+    }
+}
