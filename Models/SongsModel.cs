aaf89cb0b35b23f6621a147efbcdeeb92a5b2905
Alejandro Lopez <al2151@mynsu.nova.edu>
2021-03-29 20:05:08 -0400

    Created Songs Model

---
+++ Models/SongsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muszilla.Models
+{
+    public class SongsModel
+    {
+        public string AddressID { get; set;}
+        public string City { get; set; }
+    }
+}
