93e1c2110e44983effa5338eaeecb3f6fc892bef
Micheal <Micheal@W10-MAZC>
2021-04-07 21:58:15 -0400

    Azure Storage access.

---
+++ Models/AzureStorageConfig.cs
@@ -0,0 +1,10 @@
+namespace Muszilla.Models
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string ImageContainer { get; set; }
+        public string ThumbnailContainer { get; set; }
+    }
+}
