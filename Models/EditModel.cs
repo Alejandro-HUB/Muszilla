c3908ec6cfb36b05ab127808d61a4157a33c9790
Alejandro Lopez <al2151@mynsu.nova.edu>
2021-04-11 00:50:20 -0400

    Started implementing update fields

---
+++ Models/EditModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Muszilla.Models
+{
+    public class EditModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must at least be {2} characters long.", MinimumLength = 6)]
+        public string Pass_word { get; set; }
+    }
+}
