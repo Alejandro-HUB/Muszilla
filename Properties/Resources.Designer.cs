39b6a5e7498c268c89458fe8ff601ae3b25cdd48
Alejandro Lopez <alzcol@outlook.com>
2022-02-12 00:09:44 -0500

    Updated connection string, added external js, and fixed some bugs

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Muszilla.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace Muszilla.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Server=tcp:muzilla2.database.windows.net,1433;Initial Catalog=muzilla;Persist Security Info=False;User ID=muzilla;Password=AleMazNat1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;.
+        ///   Looks up a localized string similar to Server=ALE-PC\SQLEXPRESS;Database=master;Trusted_Connection=True;MultipleActiveResultSets=True.
         /// </summary>
         internal static string ConnectionString {
             get {
