0dee35cad6c23e8c7923ce4cf1bf8e3966675f1d
Micheal <Micheal@W10-MAZC>
2022-06-05 18:47:23 -0400

    MajorChanges summarized: *Names of ActionResults were updated to match new view folder *New folder added to home folder to organize sign in pages *delted some comments from some js files that were causing a small error *created a new css folder that contains all cs and pictures for all other pages to use *start up was modified to fit new view names *signing and sign up work as intended in the appropriate view *homepages shows user name and picture

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace Alody
             app.UseCookiePolicy();
             app.UseStaticFiles();
             app.UseSession();
-
+            
             app.UseRouting();
             app.UseAuthorization();
 
@@ -59,7 +59,7 @@ namespace Alody
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id=UrlParameter.Optional}");
             });
         }
     }
